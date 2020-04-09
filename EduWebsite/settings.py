362affa4acf8a1b314941578b82634f21de310f2
RezaBasereh <rzbasereh1@gmail.com>
2020-04-09 17:25:07 +0330

    remove admin app and create main app

--- EduWebsite/settings.py
+++ EduWebsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'student.apps.StudentConfig',
-    'admin.apps.AdminConfig',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
