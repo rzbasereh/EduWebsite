29e89889029180c7366d9596684505c3eb08c558
RezaBasereh <rzbasereh1@gmail.com>
2020-04-10 07:54:07 +0330

    add global static files and setup simple login ui

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('login/', views.login, name="login"),
+    path('recover-pass/', views.recoverPassword, name="recoverPassword"),
 ]
