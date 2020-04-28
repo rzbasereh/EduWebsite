416ef30011e6410d39ce2058f0ff6f53910e15d7
sadra <m.sad.ra.972@gmail.com>
2020-04-28 06:24:04 +0430

    exam page!

--- static/js/script.js
+++ static/js/script.js
@@ -3,5 +3,6 @@ $(document).ready(function () {
         $(".sidebar").toggleClass('showSlideBar');
         $(".sidebar > ul > a > li > span:last-child").toggleClass('removeText');
         $(".topDrive").toggleClass('changeTopDrive');
+        $(".Page-Body").toggleClass('max-width');
     });
 });
