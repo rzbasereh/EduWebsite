ab2c696d30f6521ecf8b56e57b573a7563b0cb43
sadra467 <m.sad.ra.972@gmail.com>
2020-07-22 16:17:38 +0430

    dfhjgkj,wefsdzx

--- teacher/static/teacher/js/script.js
+++ teacher/static/teacher/js/script.js
@@ -342,12 +342,12 @@ $(document).ready(function () {
             $(this).html(`<svg class="bi bi-pause-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z"/>
                           </svg>`);
-            $("#executeExam").modal();
+            // $("#executeExam").modal();
 
         } else {
             $(this).html(`<svg class="bi bi-play-fill" width="1em" height="1em" viewBox="0 0 16 16"fill="currentColor" 
                                         xmlns="http://www.w3.org/2000/svg" data-toggle="modal" 
-                                        data-target="#exampleModal">
+                                        data-target="#selectDestination">
                                  <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
                           </svg>`);
             $(this).attr("data-original-title", "<p class='tool'>اجرای ازمون</p>");
